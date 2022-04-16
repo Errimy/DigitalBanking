a26bbc867c77d7a82ce2f54e8a1cd8bb2bbd6bb0
IsmailHaf <53225171+IsmailHaf@users.noreply.github.com>
2022-04-16 11:22:03 +0000

    test

--- src/test/java/com/example/digitalbanking/DigitalBankingApplicationTests.java
+++ src/test/java/com/example/digitalbanking/DigitalBankingApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.digitalbanking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 @SpringBootTest
 class DigitalBankingApplicationTests {
 
