fce2df8a28660081df17e90b44801167d8615870
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-04-19 21:18:20 +0000

    getters and setters

--- src/main/java/com/example/digitalbanking/entities/Client.java
+++ src/main/java/com/example/digitalbanking/entities/Client.java
@@ -14,4 +14,54 @@ public class Client implements Serializable {
     private Long Tel;
     private String Adresse;
     private Collection<Compte> comptes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public Long getTel() {
+        return Tel;
+    }
+
+    public void setTel(Long tel) {
+        Tel = tel;
+    }
+
+    public String getAdresse() {
+        return Adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        Adresse = adresse;
+    }
+
+    public Collection<Compte> getComptes() {
+        return comptes;
+    }
+
+    public void setComptes(Collection<Compte> comptes) {
+        this.comptes = comptes;
+    }
+
+
 }
